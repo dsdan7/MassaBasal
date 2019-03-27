336127546cfefef88fd5327952b47e3862eb2caf
dsdan7 <dsdsdan7@gmail.com>
2019-03-28 01:14:11 +0900

    Tudo funcionando do jeito brasileiro

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -1,2 +1,34 @@
-// This is a JavaScript file
+$(document).on('click','#butao', function(){
+  var result;
+    var idade = $(".idade").val();
+    var genero = $(".genero").val();
+    var altura = $(".altura").val();
+    var peso = $(".peso").val();
+    var atividade = $(".atividade").val();
+    if(atividade==1){
+          atividade=1.2;
+    }
+    if(atividade==2){
+          atividade=1.375;
+    }
+    if(atividade==3){
+          atividade=1.55;
+    }
+    if(atividade==4){
+         atividade=1.725;
+    }
+    if(atividade==5){
+         atividade=1.9;
+    }
+    if(genero=="Masculino"){
+       /*Taxa de atividade x {66 + [(13,7 x Peso(kg)) + ( 5 x Altura(cm)) - (6,8 x Idade(anos))]}*/
+       result=atividade*(66+(13.7*peso)+(5*altura)-(6.8*idade));  
+    }else if(genero=="Feminino"){
+    /*Taxa de atividade x {655 + [(9,6 x Peso(kg)) + (1,8 x Altura(cm)) - (4,7 x Idade(anos))]}  */
+    result=atividade*(655+(9.6*peso)+(1.8*altura)-(4.7*idade));
+    }
+    
+    $("#result").val(result);
+    
+});
 
